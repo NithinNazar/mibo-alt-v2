9d905b5c42371517c7abbd5179cef8738e31219a
NithinNazar <nithinanazar@gmail.com>
2025-08-27 19:55:29 +0530

    ui update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mibo-alt-v2/',
-})
+  base: "/mibo-alt-v2/",
+});
